f62f3084e4a9977af4773bb1a52543db30fb25cf
Dario Hendrix <dariohendrix@hotmail.it>
2026-03-05 14:09:45 +0100

    aggiunto middleware role, testato su rotte admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,9 +8,10 @@ class AdminController extends Controller
 {
     public function dashboard()
     {
-        if(! auth()->user()->isAdmin()){
-            abort(403, 'Non sei autorizzato a vedere questa pagina');
-        }
-        return view('admin.dashboard');
+        // if(! auth()->user()->isAdmin()){
+        //     abort(403, 'Non sei autorizzato a vedere questa pagina');
+        // }
+        // return view('admin.dashboard');
     }
 }
+// ** funzione spostata su RoleMiddleware **
--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,22 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})
+->middleware(['auth', 'verified'])
+->name('dashboard');
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])
+    ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+    ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+    ->name('profile.destroy');
 });
 
-Route::get('/admin', [AdminController::class, 'dashboard'])->middleware(['auth'])->name('admin.dashboard');
+Route::get('/admin', [AdminController::class, 'dashboard'])
+->middleware(['auth', 'role:admin'])
+->name('admin.dashboard');
 
 require __DIR__.'/auth.php';
